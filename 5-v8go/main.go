91dbeeeeee6e6929d6fc6e577f61c11032c3e0c4
leafney <babycoolzx@126.com>
2023-05-31 17:26:10 +0800

    feat: v8go

---
+++ 5-v8go/main.go
@@ -0,0 +1,13 @@
+/**
+ * @Author:      leafney
+ * @GitHub:      https://github.com/leafney
+ * @Project:     chromedp-rod-test
+ * @Date:        2023-05-31 17:24
+ * @Description:
+ */
+
+package main
+
+func main() {
+
+}
---
+++ docs/v8go.md
@@ -0,0 +1,9 @@
+# v8go
+
+- [rogchap/v8go: Execute JavaScript from Go](https://github.com/rogchap/v8go) 
+
+
+```
+import v8 "rogchap.com/v8go"
+```
+
